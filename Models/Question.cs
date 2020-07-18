bce1e47b8f7937b68f5f5cc43c0a9366a88cadee
abhishek porwal <abhi.porwal0@gmail.com>
2020-07-18 19:43:16 +0530

    model and repository

---
+++ Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models
+{
+    public class Question
+    {
+
+        public int Questionnumber { set; get; }
+        public string Questions { set; get; }
+        public int Correcanswer { set; get; }
+    }
+}
---
+++ Repository/QuestionRepostory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Dapper;
+using MySql.Data.MySqlClient;
+using Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class QuestionRepostory
+    {
+        private readonly MySqlConnection _conn;
+        private string connectionString = "Server=127.0.0.1;Database=kbc;Uid=root;Pwd=root;";
+
+
+        public QuestionRepostory()
+        {
+            _conn= new MySqlConnection(connectionString);
+        }
+        
+
+        public List<Question> GetQuestions()
+        {
+            try
+            {
+
+                var sql = "SELECT * FROM kbc.Question;";
+                var result = this._conn.Query<Question>(sql).ToList<Question>();
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
