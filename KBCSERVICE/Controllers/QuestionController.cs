9492010ec5580f8de2658320b3a9071fd5bef79c
abhishek porwal <abhi.porwal0@gmail.com>
2020-07-18 19:14:21 +0530

    service

---
+++ KBCSERVICE/Controllers/QuestionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Repository;
+
+namespace KBCSERVICE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        [HttpGet]
+        public List<Question> Get()
+        {
+
+            QuestionRepostory _QuestionRepostory = new QuestionRepostory();
+
+
+            List<Question> question = _QuestionRepostory.GetQuestions();
+            return question;
+        }
+
+    }
+}
